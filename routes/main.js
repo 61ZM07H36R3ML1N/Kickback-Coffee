85c9f296ca464e031d5e15bcc3b6c4d84b384bf9
BT <brianthomasjamiel@gmaail.com>
2021-10-02 12:40:26 -0700

    added mustache tagging for view render

--- routes/main.js
+++ routes/main.js
@@ -2,9 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
+    const data = {
+        title: "Kickback-Coffee Bar and Lounge"
+    }
 
-    res.render('home')
-    
+    res.render('home', data)
+
 })
 
 module.exports = router
