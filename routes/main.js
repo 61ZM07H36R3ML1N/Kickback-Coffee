8c93c2d28ad57ed8e3d55a292eada013994b937a
BT <brianthomasjamiel@gmaail.com>
2021-10-03 11:11:00 -0700

    changed code for globals

--- routes/main.js
+++ routes/main.js
@@ -2,21 +2,15 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    const data = {
-        title: "Kickback-Coffee Bar and Lounge",
-        description: "Welcome to the Kickback-Coffee Bar and Lounge! We aim to create exquisite coffee and lounge style drinks "
-    }
+    const data = req.context
 
     res.render('home', data)
 
 })
 
 router.get('/blog', (req, res, next) => {
-    const data = {
-        title: "Kickback-Coffee Bar and Lounge",
-        description: "Welcome to the Kickback-Coffee Bar and Lounge! We aim to create exquisite coffee and lounge style drinks "
-    }
-    
+    const data = req.context
+
     res.render('blog', data)
 
 })
